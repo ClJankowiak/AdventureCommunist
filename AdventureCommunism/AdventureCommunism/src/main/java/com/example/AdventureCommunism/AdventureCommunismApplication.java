73d594ef8ee69408a7671e51ec8fb15da0c02f75
martinBorde <martin.borde@etud.univ-jfc.fr>
2021-06-08 09:58:11 +0200

    PUSH first project server

---
+++ AdventureCommunism/AdventureCommunism/src/main/java/com/example/AdventureCommunism/AdventureCommunismApplication.java
@@ -0,0 +1,13 @@
+package com.example.AdventureCommunism;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdventureCommunismApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdventureCommunismApplication.class, args);
+	}
+
+}
---
+++ AdventureCommunism/AdventureCommunism/src/test/java/com/example/AdventureCommunism/AdventureCommunismApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AdventureCommunism;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdventureCommunismApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
